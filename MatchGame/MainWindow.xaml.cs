6354ae77480543740de15b22634086f40005ae61
Gilen Negash <gilen.negash@gmail.com>
2022-01-13 00:07:41 +0300

    color

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -38,7 +38,7 @@ namespace MatchGame
         {
             tenthsOfSecondsElapsed++;
             timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
-            if(matchesFound == 16)
+            if(matchesFound == 18)
             {
                 timer.Stop();
                 timeTextBlock.Text = timeTextBlock.Text + " - Play again?";
@@ -71,8 +71,8 @@ namespace MatchGame
             };
 
             Random random = new Random();
-            
 
+            
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
                 if (textBlock.Name != "timeTextBlock")
@@ -82,11 +82,70 @@ namespace MatchGame
                     string nextEmoji = animalEmoji[index];
                     textBlock.Text = nextEmoji;
                     animalEmoji.RemoveAt(index);
+
+                    switch (index)
+                    {
+                        case < 2:
+                            textBlock.Foreground = Brushes.Crimson;
+                            break;
+                        case < 4:
+                            textBlock.Foreground = Brushes.Blue;
+                            break;
+                        case < 6:
+                            textBlock.Foreground = Brushes.YellowGreen;
+                            break;
+                        case < 8:
+                            textBlock.Foreground = Brushes.DarkOrange;
+                            break;
+                        case < 10:
+                            textBlock.Foreground = Brushes.Brown;
+                            break;
+                        case < 12:
+                            textBlock.Foreground = Brushes.OrangeRed;
+                            break;
+                        case < 14:
+                            textBlock.Foreground = Brushes.DeepPink;
+                            break;
+                        case < 16:
+                            textBlock.Foreground = Brushes.Purple;
+                            break;
+                        case < 18:
+                            textBlock.Foreground = Brushes.BurlyWood;
+                            break;
+                        case < 20:
+                            textBlock.Foreground = Brushes.DarkViolet;
+                            break;
+                        case < 22:
+                            textBlock.Foreground = Brushes.Lime;
+                            break;
+                        case < 24:
+                            textBlock.Foreground = Brushes.Maroon;
+                            break;
+                        case < 26:
+                            textBlock.Foreground = Brushes.Aqua;
+                            break;
+                        case < 28:
+                            textBlock.Foreground = Brushes.Sienna;
+                            break;
+                        case < 30:
+                            textBlock.Foreground = Brushes.MistyRose;
+                            break;
+                        case < 32:
+                            textBlock.Foreground = Brushes.IndianRed;
+                            break;
+                        case < 34:
+                            textBlock.Foreground = Brushes.PaleTurquoise;
+                            break;
+                        default:
+                            textBlock.Foreground = Brushes.Black;
+                            break;
+
+                    }
+
+                    
                 }
                 
             }
-            timer.Start();
-            tenthsOfSecondsElapsed = 0;
             matchesFound = 0;
             //throw new NotImplementedException();
         }
@@ -117,9 +176,13 @@ namespace MatchGame
 
         private void timeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if(matchesFound == 8)
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            
+            if (matchesFound == 18)
             {
                 SetUpGame();
+
             }
 
         }
